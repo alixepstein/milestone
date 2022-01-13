8a92109c9146e18171a6e8d2ce0af0cf22117f65
alixepstein <97540392+alixepstein@users.noreply.github.com>
2022-01-12 20:47:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
-import pandas as pdimport requestsimport json
+
+import pandas as pd
+
+import requests
+
+import json
 
 from bokeh.plotting import figure, show
 
 from bokeh.io import output_notebook
-st.title('Stock prices')symbol_input = st.text_input(label="Ticker", help="Enter the stock ticker symbol")
+
+st.title('Stock prices')
+
+
+
+symbol_input = st.text_input(label="Ticker", help="Enter the stock ticker symbol")
 
 
 payload = {'function': 'TIME_SERIES_DAILY', 'symbol': symbol_input, 'apikey': 'GNN6K0ZEGN5RICF1'}
